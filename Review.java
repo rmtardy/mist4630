e364c399db0d6504f3ca05eba1790b6723383064
rmtardy <mtardy@live.com>
2015-12-04 13:59:26 -0500

    Create Review.java

---
+++ Review.java
@@ -0,0 +1,39 @@
+package model;
+
+public class Review {
+	private int reviewID;
+	private String content;
+	private int typeID;
+	
+	public Review(){
+		this.reviewID = 1;
+		this.content = "None";
+		this.typeID = 1;
+		
+	}
+
+	public int getReviewID() {
+		return reviewID;
+	}
+
+	public void setReviewID(int reviewID) {
+		this.reviewID = reviewID;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public int getTypeID() {
+		return typeID;
+	}
+
+	public void setTypeID(int typeID) {
+		this.typeID = typeID;
+	}
+
+}
