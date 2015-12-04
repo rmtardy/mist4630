fb9d75e8d40ae229131a03e74cc5b0545051c390
rmtardy <mtardy@live.com>
2015-12-04 13:52:41 -0500

    Create Sport.java

---
+++ Sport.java
@@ -0,0 +1,31 @@
+package model;
+
+public class Sport {
+	
+	private int sportID;
+	private String sportName;
+		
+		public Sport(){
+			this.sportID = 1;
+			this.sportName = "No Name";
+		}
+
+		public int getSportID() {
+			return sportID;
+		}
+
+		public void setSportID(int sportID) {
+			this.sportID = sportID;
+		}
+
+		public String getSportName() {
+			return sportName;
+		}
+
+		public void setSportName(String sportName) {
+			this.sportName = sportName;
+		}
+
+	}
+
+
