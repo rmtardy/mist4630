686e7fc013239dc3dadd804cccbd9b8fb7637208
rmtardy <mtardy@live.com>
2015-12-04 13:53:18 -0500

    Create Coach.java

---
+++ Coach.java
@@ -0,0 +1,46 @@
+package model;
+
+public class Coach {
+
+	public int coachID;
+	public String coachFName;
+	public String coachLName;
+	
+	
+	public Coach(){
+		
+		this.coachID = 1;
+		this.coachFName = "No First Name";
+		this.coachLName = "No Last Name";
+	
+}
+
+
+	public int getCoachID() {
+		return coachID;
+	}
+
+
+	public void setCoachID(int coachID) {
+		this.coachID = coachID;
+	}
+
+
+	public String getCoachFName() {
+		return coachFName;
+	}
+
+
+	public void setCoachFName(String coachFName) {
+		this.coachFName = coachFName;
+	}
+
+
+	public String getCoachLName() {
+		return coachLName;
+	}
+
+
+	public void setCoachLName(String coachLName) {
+		this.coachLName = coachLName;
+	}
