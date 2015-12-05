8cfb75ff7c1dc1930ba2eba65e77154100c24c04
rmtardy <mtardy@live.com>
2015-12-04 21:52:59 -0500

    Create DeleteQuery.java

---
+++ DeleteQuery.java
@@ -0,0 +1,58 @@
+package dbHelpers;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class DeleteQuery {
+	
+	private Connection connection;
+	
+	public DeleteQuery(String dbName, String uname, String pwd){
+		
+		String url = "jdbc:mysql://localhost:3306/"+dbName;
+		
+		try {
+			Class.forName("com.mysql.jdbc.Driver").newInstance();
+			this.connection = DriverManager.getConnection(url, uname, pwd);
+		} catch (InstantiationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public void doDelete(String coach){
+		// set up a String to hold our query
+		String query = "delete from coach where coachID = ?";
+		
+		// create a preparedstatement using our query string
+		try {
+			PreparedStatement ps = connection.prepareStatement(query);
+			
+			// fill in the preparedstatement
+			ps.setString(1, coachID);
+			
+			// execute the query
+			ps.executeUpdate();
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+		
+		
+	}
+
+}
