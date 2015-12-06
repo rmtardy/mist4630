0562d5672d3c9ef03c8980313481e5bb468ed501
rmtardy <mtardy@live.com>
2015-12-06 17:52:39 -0500

    Update Player.java

--- Player.java
+++ Player.java
@@ -13,8 +13,12 @@ public class Player {
 		this.playerFName = "No First Name";
 		this.playerLName = "No Last Name";
 	}
-
-
+	public Player(int playerID, String playerFName, String playerLName) {
+		this.playerID = playerID;
+		this.playerFName = playerFName;
+		this.playerLName = playerLName;
+	}
+	
 	public int getPlayerID() {
 		return playerID;
 	}
