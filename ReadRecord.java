32c78a28f99550e59b89257c6f4c1c2a7d25870a
rmtardy <mtardy@live.com>
2015-12-08 10:28:47 -0500

    Update ReadRecord.java

--- ReadRecord.java
+++ ReadRecord.java
@@ -7,6 +7,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import model.Coach;
+import model.Review;
 
 public class ReadRecord {
 	
@@ -14,9 +15,10 @@ public class ReadRecord {
 	private ResultSet results;
 	
 	private Coach coach = new Coach();
-	private String coachID;
+	private int coachID;
 	
-	public ReadRecord(String dbName, String uname, String pwd, String coachID){
+	
+	public ReadRecord(String dbName, String uname, String pwd, int coachID){
 		
 		String url = "jdbc:mysql://localhost:3306/" + dbName;
 		this.coachID = coachID;
@@ -42,12 +44,12 @@ public class ReadRecord {
 	}
 	
 	public void doRead(){
-		String query = "select * from products where SKU = ?";
+		String query = "select * from coach where coachID = ?";
 		
 		try {
 			PreparedStatement ps = connection.prepareStatement(query);
 			
-			ps.setString(1, this.coachID);
+			ps.setInt(1, this.coachID);
 			
 			this.results = ps.executeQuery();
 			
@@ -67,6 +69,107 @@ public class ReadRecord {
 		return this.coach;
 	}
 	
+	public void coachProfileJoin(){
+		String query = "select reviewID, content, sport, coachLName from review,coach where review.coach_coachID = ? and coach.coachID = ?";
+		
+		
+		try {
+			PreparedStatement ps = this.connection.prepareStatement(query);
+			ps.setInt(1, this.coachID);
+			ps.setInt(2, this.coachID);
+			this.results= ps.executeQuery();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+	
+	public String getCoachReviewHTMLTable(){
+		String tableR ="";
+		try {
+		this.results.next();
+		tableR += "Coach:" + this.results.getString("coachLName");
+		tableR += "<table border=1>";
+		do{
+				Review review = new Review();
+				review.setReviewID(this.results.getInt("reviewID"));
+				review.setContent(this.results.getString("content"));
+				review.setSport(this.results.getString("sport"));
+				
+				
+				
+				tableR +="<tr>";
+				tableR +="<td>";
+				tableR += "<a href = coachProfile.jsp>";
+				tableR +=  review.getReviewID();
+				tableR += "</a>";
+				tableR +="</td>";
+				tableR +="<td>";
+				tableR += review.getContent();
+				tableR +="</td>";
+				tableR +="<td>";
+				tableR += review.getSport(); 
+				tableR +="</td>";
+//				table +="<td class='edit'>";
+//			   table += "<a href=update?coachID=" + coach.getCoachID() + ">update</a>";
+//				table +="</td>";
+//				table += "<td class='edit'>";
+//				table += "<a href=delete?coachID=" + coach.getCoachID() + ">delete</a>";
+//				table +="</td>";
+				tableR +="</tr>";
+				
+			} while (results.next());
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		tableR += "</table>";
+		return tableR;
+	}
+	
+	public String getTeamReviewHTMLTable(){
+		String tableR ="";
+		try {
+		this.results.next();
+		//tableR += "Coach:" + this.results.getString("coachLName");
+		tableR += "<table border=1>";
+		do{
+				Review review = new Review();
+				review.setReviewID(this.results.getInt("reviewID"));
+				review.setContent(this.results.getString("content"));
+				review.setSport(this.results.getString("sport"));
+				
+				
+				
+				tableR +="<tr>";
+				tableR +="<td>";
+				tableR += "<a href = coachProfile.jsp>";
+				tableR +=  review.getReviewID();
+				tableR += "</a>";
+				tableR +="</td>";
+				tableR +="<td>";
+				tableR += review.getContent();
+				tableR +="</td>";
+				tableR +="<td>";
+				tableR += review.getSport(); 
+				tableR +="</td>";
+//				table +="<td class='edit'>";
+//			   table += "<a href=update?coachID=" + coach.getCoachID() + ">update</a>";
+//				table +="</td>";
+//				table += "<td class='edit'>";
+//				table += "<a href=delete?coachID=" + coach.getCoachID() + ">delete</a>";
+//				table +="</td>";
+				tableR +="</tr>";
+				
+			} while (results.next());
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		tableR += "</table>";
+		return tableR;
+	}
 	
 
 }
